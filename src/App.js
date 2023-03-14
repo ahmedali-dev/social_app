379cc47a20af778c3c36ff55afecd3dc1a595e1f
ahmedali-dev <mohamedali850100@gmail.com>
2023-03-14 19:02:05 +0200

    add new statment method

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import Header from "./components/Header/Header";
 import { useState } from "react";
 
 const App = (props) => {
-  return (
-    <>
-      {sessionStorage.getItem("token") ? <Header /> : null}
-      {/* main content */}
-      <main className="bottomspace topspace">{props.children}</main>
-    </>
-  );
+  const activeLogin = () => {
+    if (sessionStorage.getItem("token")) {
+      return (
+        <>
+          <Header />
+          <div className="bottomspace"></div>
+          <main className="main">{props.children}</main>
+          <div className="topspace"></div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <main className="main">{props.children}</main>
+        </>
+      );
+    }
+  };
+  return <>{activeLogin()}</>;
 };
 
 export default App;
